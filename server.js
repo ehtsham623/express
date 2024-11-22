28dcade163840f38c997efc8c82645a0684b3ff6
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-23 02:07:07 +0500

    env,basic api, serve files, static file serve, json send

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const port = process.env.PORT || 8000;
 
+//serves static file ,eg: localhost:8000/main.html
+app.use(express.static(path.join(__dirname, "public")));
+
+///default
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+///send file
 app.get("/about", (req, res) => {
-  res.send("about");
+  res.sendFile(path.join(__dirname, "public", "aboutPage.html"));
 });
 
-// starts a simple http server locally on port 3000
-app.listen(8000, () => {
+///json example
+let users = [
+  { id: "1", name: "data1" },
+  { id: "2", name: "data2" },
+  { id: "3", name: "data3" },
+];
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+///listen server
+app.listen(port, () => {
   console.log("Listening on port 8000");
 });
-
-// run with `node server.mjs`
