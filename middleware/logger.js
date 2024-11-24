ed479ba308823d0de2a64b2be21d56dce55a5b0f
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-24 05:43:54 +0500

    error handle and colors for logging

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,16 @@
+import colors from "colors";
 const logger = (req, res, next) => {
+  const colorsMethod = {
+    GET: "green",
+    POST: "blue",
+    PUT: "yellow",
+    DELETE: "red",
+  };
+  const color = colorsMethod[req.method] || "white";
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`,
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`[
+      color
+    ],
   );
   next();
 };
